9032d36de4b603208a000dbf7d3e7898e3ca110c
Роман Данилевич <dwordchanel@gmail.com>
2020-05-03 23:13:56 +0300

    Add project files.

---
+++ OOP_lab_3_11_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_lab_3_11_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть назву предмету (OOP/AP).");
+
+            string name = Console.ReadLine();
+
+            try
+            {
+                StreamReader fromFile = new StreamReader(name + ".txt");
+
+                string str = fromFile.ReadToEnd();
+
+                Console.WriteLine(str);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Введено неправильну назву предмету.");
+            }
+        }
+    }
+}
